2d656deef268242dfdd1dcf1c2dd7155f3ae441c
James Thompson <jthompson@zipcoders-MacBook-Pro.local>
2020-03-06 10:43:45 -0500

    update doc strings

--- exponentiator.py
+++ exponentiator.py
@@ -1,11 +1,17 @@
 def exponentiate(x, y):
-"""takes x and raises it to the value of y """	
+
+"""
+Takes x and raises it to the value of y.
+"""	
 	return	(x ** y)
 
 def raise_to_fourth_power(y):
-""" Takes the value Z and runs 
+
+""" 
+Takes the value Z and runs 
 it through the exponentiate function 
-with predetermined value of 4 as the power """	
+with predetermined value of 4 as the power.
+"""	
 	return exponentiate(y, 4)	
 
 square = lambda y: exponentiate(y, 2)
